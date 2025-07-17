024e851109e6e6bffaf4d26e1e8f1c5ac290ce28
Miguel Castro <mcastro_23@alu.uabcs.mx>
2025-07-16 20:30:55 -0700

    Solution to exercise 9: sum all

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR"
+    }
+    if(a < 0 || b < 0) {
+        return "ERROR"
+    }
+    let lowerLimit = Math.min(a, b)
+    let upperLimit = Math.max(a, b)
+    
+    let sum = 0
+    for(let n = lowerLimit; n <= upperLimit; n++) {
+        sum += n
+    }
+    return sum
 };
 
 // Do not edit below this line
